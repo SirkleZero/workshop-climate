d5f96583a05236e9683d0af55a432c140599c146
Joshua Gall <josh@imtraum.com>
2018-11-29 23:10:51 -0600

    added in a stub for the new sensor manager

--- workshop-climate-sensors.ino
+++ workshop-climate-sensors.ino
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 
 #include "workshop-climate-lib.h" // unsure exactly why this has to be here for this to compile. without it, the sub-directory .h files aren't found. Probably has something to do with not finding the library if nothing is loaded from the root of the src folder.
+#include "Sensors\SensorManager.h"
 #include "Sensors\SensorData.h"
 #include "Sensors\PMS5003Frame.h"
 #include "Sensors\BME280Proxy.h"
@@ -10,13 +11,13 @@
 
 using namespace Display;
 using namespace Sensors;
-using namespace Sensors::BME280;
-using namespace Sensors::PMS5003;
 using namespace TX;
 
 const unsigned long sampleFrequency = 60000; // ms (once per minute)
 bool isFirstLoop = true;
 SensorData data;
+
+SensorManager manager(AvailableSensors::All);
 
 BME280Proxy climateProxy(TemperatureUnit::F);
 PMS5003Proxy particleProxy;
