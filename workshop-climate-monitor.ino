4bb874f6ae503a27029b00a90cf3b3d5b3b7be06
Joshua Gall <josh@imtraum.com>
2019-11-02 18:55:21 -0500

    Removed the relay manager. The monitor is passively engaged in the system; it does not manage humidification or dehumidification

--- workshop-climate-monitor.ino
+++ workshop-climate-monitor.ino
@@ -7,7 +7,6 @@
 
 #include "workshop-climate-lib.h" // unsure exactly why this has to be here for this to compile. without it, the sub-directory .h files aren't found. Probably has something to do with not finding the library if nothing is loaded from the root of the src folder.
 #include "Sensors\SensorData.h"
-#include "Relay\HumidityRelayManager.h"
 #include "RX\RFM69RXProxy.h"
 #include "RX\SensorTransmissionResult.h"
 #include "TX\AdafruitIOProxy.h"
@@ -19,7 +18,6 @@
 
 using namespace Configuration;
 using namespace Display;
-using namespace Relay;
 using namespace RX;
 using namespace Sensors;
 using namespace TX;
@@ -35,7 +33,6 @@ InitializationResult internetEnabled;
 // objects that handle functionality
 SDCardProxy sdCard;
 RXTFTFeatherwingProxy display;
-HumidityRelayManager relayManager;
 RFM69RXProxy radio;
 AdafruitIOProxy httpClient;
 
@@ -43,9 +40,8 @@ AdafruitIOProxy httpClient;
 we need our modules in the following priority order:
 1. SD Card - Contains all of our configuration. Can't run without this information.
 2. The Display - we can technically function without a display, but we're going to use it to display errors, so uh, it's required.
-3. Relay Manager - no point in doing any of this if the relay handling isn't there
-4. The RFM69 Radio - Sends us data. Without it, again, no point.
-5. The Internet / adafruit - we can function without this, it's the only component not really required.
+3. The RFM69 Radio - Sends us data. Without it, again, no point.
+4. The Internet / adafruit - we can function without this, it's the only component not really required.
 */
 void setup()
 {
@@ -68,40 +64,29 @@ void setup()
 			sdCard.LoadConfiguration(&controllerConfiguration);
 			Serial.println("configuration loaded");
 
-			InitializationResult relayManagerResult = relayManager.Initialize(&controllerConfiguration);
-			if (relayManagerResult.IsSuccessful)
+			Serial.println("relay initialized");
+
+			InitializationResult radioResult = radio.Initialize();
+			if (radioResult.IsSuccessful)
 			{
-				Serial.println("relay initialized");
+				Serial.println("Radio initialized");
 
-				InitializationResult radioResult = radio.Initialize();
-				if (radioResult.IsSuccessful)
+				internetEnabled = httpClient.Initialize(&secrets);
+				if (internetEnabled.IsSuccessful)
 				{
-					Serial.println("Radio initialized");
-
-					internetEnabled = httpClient.Initialize(&secrets);
-					if (internetEnabled.IsSuccessful)
-					{
-						Serial.println("loaded internets");
-					}
-					else
-					{
-						Serial.println("failed to load internets");
-					}
+					Serial.println("loaded internets");
 				}
 				else
 				{
-					systemRunnable = false;
-					display.PrintError(radioResult.ErrorMessage);
-					Serial.println(radioResult.ErrorMessage);
-					sdCard.LogMessage(radioResult.ErrorMessage);
+					Serial.println("failed to load internets");
 				}
 			}
 			else
 			{
 				systemRunnable = false;
-				display.PrintError(relayManagerResult.ErrorMessage);
-				Serial.println(relayManagerResult.ErrorMessage);
-				sdCard.LogMessage(relayManagerResult.ErrorMessage);
+				display.PrintError(radioResult.ErrorMessage);
+				Serial.println(radioResult.ErrorMessage);
+				sdCard.LogMessage(radioResult.ErrorMessage);
 			}
 		}
 
@@ -120,13 +105,6 @@ void loop()
 		// reset the watchdog with each loop iteration. If the loop hangs, the watchdog will reset the device.
 		Watchdog.reset();
 
-		/*
-		Use the emergency shutoff function to shut off the relays if a pre-determined time amount has lapsed. All of this logic is within this method, no other calls are necessary. The KeepAlive() method is essentially a dead man switch that this method uses to either keep things going, or, if the sensor array functionality doesn't transmit anything or we don't receive anything, we shut down power to all our devices.
-
-		This is a safety thing.
-		*/
-		relayManager.EmergencyShutoff();
-
 		// !!! CRITICAL !!!
 		// the rxProxy listen function needs to execute as often as possible to not miss any messages or acknowledgements. it would be bad to have the loop have a delay call in it, messages will be lost.
 		// DO NOT put a delay call in the loop function!
@@ -138,7 +116,6 @@ void loop()
 			display.PrintSensors(result.Data);
 			display.PrintFreeMemory(freeMemory());
 
-			relayManager.AdjustClimate(result.Data);
 			Watchdog.reset();
 
 			// if the internet isn't working for some reason, don't bother trying to upload anything.
@@ -162,9 +139,6 @@ void loop()
 			if (!resetResult.IsSuccessful)
 			{
 				// something didn't work here, so let's...
-				// 1. Shut down due to an error. This should keep us from turning on a device and then having an error prevent another cycle from running. This prevents us from finding the system in a permanently on state; a permanently off state is preferable by far.
-				// 2. Display an error message
-				relayManager.ShutDownError();
 
 				display.PrintError(resetResult.ErrorMessage);
 				Serial.println(resetResult.ErrorMessage);
