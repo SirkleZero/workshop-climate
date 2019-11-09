339b55ff29fd2540bf21b07c54bfc5676f8509c6
Joshua Gall <josh@imtraum.com>
2019-11-09 00:11:23 -0600

    working to update the display and putting things in place to include controller logic

--- workshop-climate-sensors.ino
+++ workshop-climate-sensors.ino
@@ -1,68 +1,105 @@
 #include <Arduino.h>
+#include <Adafruit_SleepyDog.h>
 
-#include "workshop-climate-lib.h" // unsure exactly why this has to be here for this to compile. without it, the sub-directory .h files aren't found. Probably has something to do with not finding the library if nothing is loaded from the root of the src folder.
-#include "Sensors\SensorManager.h"
+/*
+unsure exactly why this has to be here for this to compile. without it, the sub-directory .h files
+aren't found. Probably has something to do with not finding the library if nothing is loaded from
+the root of the src folder.
+*/
+#include "workshop-climate-lib.h"
+
 #include "Sensors\SensorData.h"
-#include "Sensors\PMS5003Frame.h"
+#include "Display\RXTFTFeatherwingProxy.h"
+#include "Configuration\SDCardProxy.h"
+#include "Configuration\ControllerConfiguration.h"
+#include "Sensors\SensorManager.h"
 #include "Sensors\BME280Proxy.h"
-#include "Sensors\PMS5003Proxy.h"
 #include "TX\RFM69TXProxy.h"
-#include "Display\FeatherOLEDProxy.h"
 
+using namespace Configuration;
 using namespace Display;
 using namespace Sensors;
 using namespace TX;
 
-static const unsigned long SampleFrequency = 15000; // every 15 seconds
-//static const unsigned long SampleFrequency = 60000; // every 60 seconds
+static const unsigned long DefaultSampleFrequency = 15000; // every 15 seconds
 bool isFirstLoop = true;
 bool systemRunnable = true;
 
-SensorManager sensorManager(AvailableSensors::BME280, TemperatureUnit::F, SampleFrequency);
+SDCardProxy sdCard;
+RXTFTFeatherwingProxy display;
+ControllerConfiguration config;
+SensorManager sensorManager(AvailableSensors::BME280, TemperatureUnit::F, DefaultSampleFrequency);
 RFM69TXProxy transmissionProxy;
-FeatherOLEDProxy displayProxy;
 SensorData data;
 
-void setup() {
-    Serial.begin(115200);
+void setup()
+{
+	Serial.begin(115200);
 
-    //while (!Serial); // MAKE SURE TO REMOVE THIS!!!
+	while (!Serial); // MAKE SURE TO REMOVE THIS!!!
 
-	if (displayProxy.Initialize().IsSuccessful)
+	if (display.Initialize().IsSuccessful)
 	{
-		displayProxy.PrintWaiting();
+		display.Clear();
+		display.DrawLayout();
+		display.PrintSensors(SensorData::EmptyData());
 
+		// Radio chip select needs to be pulled up per this thread
+		// https://forums.adafruit.com/viewtopic.php?f=47&t=120223&start=15
+		//pinMode(8, INPUT_PULLUP);
+		// OR
+		// initialize the radio first.
+		// no idea why this causes the sd card to fail initialization, but if we don't
+		// do one of these options, we can't read the sd card...... lame.
+		InitializationResult tr = transmissionProxy.Initialize();
 		InitializationResult smr = sensorManager.Initialize();
-		if (smr.IsSuccessful)
+		InitializationResult sdr = sdCard.Initialize();
+		if (sdr.IsSuccessful)
 		{
-			systemRunnable = transmissionProxy.Initialize().IsSuccessful;
+			sdCard.LoadConfiguration(&config);
 		}
-		else
-		{
-			displayProxy.PrintError(smr.ErrorMessage);
-		}
+
+		systemRunnable = smr.IsSuccessful && tr.IsSuccessful && sdr.IsSuccessful;
+		systemRunnable = true;
+
+		// IMPORTANT! Turn on the watch dog timer and enable at the maximum value. For the M0 
+		// this is approximately 16 seconds, after which the watch dog will restart the device.
+		// This exists purely as a stability mechanism to mitigate device lockups / hangs / etc.
+		Watchdog.enable();
+		//sdCard.LogMessage(F("Watchdog timer enabled during device setup."));
+		Serial.println(F("Watchdog timer enabled during device setup."));
 	}
 }
 
-void loop() {
+void loop()
+{
 	// if all the checks from the Setup method ran successfully, we're good to run; otherwise, print an error message.
 	if (systemRunnable)
 	{
+		// reset the watchdog with each loop iteration. If the loop hangs, the watchdog will reset the device.
+		Watchdog.reset();
+
 		// the manager uses a configurable timer, so call this method as often as possible.
 		if (sensorManager.ReadSensors(&data))
 		{
+			Watchdog.reset();
+
 			// clear the waiting message from the display
 			if (isFirstLoop)
 			{
-				displayProxy.Clear();
+				//displayProxy.Clear();
 			}
 
 			// print the information from the sensors.
-			displayProxy.PrintSensors(data);
+			display.PrintSensors(data);
+			//data.PrintDebug();
 
 			// use the radio and transmit the data. when done, print some information about how the transmission went.
 			TXResult result = transmissionProxy.Transmit(data);
-			displayProxy.PrintTransmissionInfo(result);
+			//displayProxy.PrintTransmissionInfo(result);
+			//result.PrintDebug();
+
+			Watchdog.reset();
 
 			isFirstLoop = false;
 		}
@@ -72,6 +109,6 @@ void loop() {
 		// the needed components of the system are not present or working, show a message
 		const __FlashStringHelper *msg = F("One or more components failed to initialize.");
 		Serial.println(msg);
-		displayProxy.PrintError(msg);
+		//displayProxy.PrintError(msg);
 	}
 }
