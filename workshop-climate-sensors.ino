414e53f9456a70637c24c2619e8330a3a8096dd1
Joshua Gall <josh@imtraum.com>
2019-11-17 10:57:44 -0600

    wip

--- workshop-climate-sensors.ino
+++ workshop-climate-sensors.ino
@@ -47,6 +47,7 @@ void setup()
 	{
 		display.Clear();
 
+		// NOTE!
 		// Radio chip select needs to be pulled up per this thread
 		// https://forums.adafruit.com/viewtopic.php?f=47&t=120223&start=15
 		//pinMode(8, INPUT_PULLUP);
